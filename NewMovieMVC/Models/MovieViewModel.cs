1f2e7989554c6e956b8c1b561d8193592db302c0
AntonioMarkovski <76123051+AntonioMarkovski@users.noreply.github.com>
2021-06-14 17:19:29 +0200

    Add project files.

---
+++ NewMovieMVC/Models/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewMovieMVC.Models
+{
+    public class MovieViewModel
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+    }
+}
